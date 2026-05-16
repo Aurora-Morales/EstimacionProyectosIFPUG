b8e1684bd0eb52411c2a2491445097a12019d676
aurora <ma5.morales.aurora@gmail.com>
2026-05-16 16:03:41 -0600

    Se agregó la seccion para agregar las transacciones del sistema.

--- src/main/java/com/uacm/estimacionifpug/estimacionproyectosifpug/HelloController.java
+++ src/main/java/com/uacm/estimacionifpug/estimacionproyectosifpug/HelloController.java
@@ -7,19 +7,46 @@ import javafx.geometry.Insets;
 
 public class HelloController {
 
-    @FXML private TextField numeroElementosFuncionales;
-    @FXML private TextField numeroElementosNoFuncionales;
-    @FXML private VBox containerFuncionales;
-    @FXML private VBox containerNoFuncionales;
+    @FXML private TextField numeroFuncionesDatosILF;
+    private int numero_funciones_datosILF;
+    @FXML private TextField numeroFuncionesDatosEIF;
+    private int numero_funciones_datosEIF;
+    @FXML private TextField numeroFuncionesTransaccionesEI;
+    @FXML private TextField numeroFuncionesTransaccionesEO;
+    @FXML private TextField numeroFuncionesTransaccionesEQ;
+    @FXML private VBox containerFuncionesDatosILF;
+    @FXML private VBox containerFuncionesDatosEIF;
+    @FXML private VBox containerFuncionesTransaccionesEI;
+    @FXML private VBox containerFuncionesTransaccionesEO;
+    @FXML private VBox containerFuncionesTransaccionesEQ;
 
+    //Acciones para agregar las tarjetas de las funciones de datos ILF y EIF
     @FXML
-    protected void onGenerarFuncionesDatosClick() {
-        generarTarjetas(numeroElementosFuncionales, containerFuncionales, "Funciones de datos");
+    protected void onGenerarFuncionesDatosILFClick() {
+        generarTarjetas(numeroFuncionesDatosILF, containerFuncionesDatosILF, "Usuarios");
+        numero_funciones_datosILF = Integer.parseInt(numeroFuncionesDatosILF.getText());
+        //System.out.println("ILF: " + numero_funciones_datosILF);
     }
 
     @FXML
-    protected void onGenerarFuncionesTransaccionesClick() {
-        generarTarjetas(numeroElementosNoFuncionales, containerNoFuncionales, "Funciones de transacciones");
+    protected void onGenerarFuncionesDatosEIFClick() {
+        generarTarjetas(numeroFuncionesDatosEIF, containerFuncionesDatosEIF, "Agregar usuario");
+        numero_funciones_datosEIF = Integer.parseInt(numeroFuncionesDatosEIF.getText());
+        //System.out.println("EIF: " + numero_funciones_datosILF);
+    }
+
+    //Acciones para agregar de las tarjetas de las funciones de las transacciones
+    @FXML
+    protected void onGenerarFuncionesTransaccionesEI(){
+        generarTarjetas(numeroFuncionesTransaccionesEI, containerFuncionesTransaccionesEI, "Agregar Usuario");
+    }
+    @FXML
+    protected void onGenerarFuncionesTransaccionesEO(){
+        generarTarjetas(numeroFuncionesTransaccionesEO, containerFuncionesTransaccionesEO, "Agregar Usuario");
+    }
+    @FXML
+    protected void onGenerarFuncionesTransaccionesEQ(){
+        generarTarjetas(numeroFuncionesTransaccionesEQ, containerFuncionesTransaccionesEQ, "Agregar Usuario");
     }
 
     private void generarTarjetas(TextField input, VBox container, String tipo) {
@@ -34,17 +61,16 @@ public class HelloController {
         }
     }
 
-    private VBox crearTarjetaEditable(String tituloDefecto) {
+    private VBox crearTarjetaEditable(String sugerencia) {
         VBox tarjeta = new VBox(8);
         tarjeta.setPadding(new Insets(10));
         tarjeta.setStyle("-fx-border-color: #abb2b9; -fx-border-radius: 5; -fx-background-color: white;");
 
         // Campo para el nombre
         TextField txtNombre = new TextField();
-        txtNombre.setPromptText("Nombre del elemento");
-        txtNombre.setText(tituloDefecto);
+        txtNombre.setPromptText("Ej: " + sugerencia);
 
-        tarjeta.getChildren().addAll(new Label("Nombre:"), txtNombre);
+        tarjeta.getChildren().addAll(new Label("Nombre del elemento:"), txtNombre);
         return tarjeta;
     }
 }
